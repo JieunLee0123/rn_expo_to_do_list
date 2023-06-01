855dd60f0d8386c6df264e241fa1f49edef8d498
jieun0123 <jieun.lee@benefitplus.kr>
2023-06-01 18:36:53 +0900

    [STYLE] StyleSheet, Platform

--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -1,16 +1,16 @@
-import { StyleSheet, Text, View, SafeAreaView, StatusBar } from 'react-native'
+import { StyleSheet, Text, View, SafeAreaView, StatusBar, Platform } from 'react-native'
 import React from 'react'
 
 const MainScreen = () => {
   return (
-    <SafeAreaView>
+    <SafeAreaView style={styles.container}>
       <StatusBar barStyle={'default'}></StatusBar>
-      <Text>To Do App</Text>
-      <View>
-        <Text>할 일</Text>
+      <Text style={styles.title}>To Do App</Text>
+      <View style={styles.listView}>
+        <Text style={styles.subTitle}>할 일</Text>
       </View>
-      <View>
-        <Text>완료된 일</Text>
+      <View style={styles.listView}>
+        <Text style={styles.subTitle}>완료된 일</Text>
       </View>
     </SafeAreaView>
   )
@@ -18,4 +18,28 @@ const MainScreen = () => {
 
 export default MainScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    padding: 15,
+  },
+  listView: {
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    ...Platform.select({
+        ios: {
+          color: 'blue',
+        },
+        android: {
+          color: 'red',
+        },
+    }),
+  },
+  subTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+});
